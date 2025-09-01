1d8a8a67c566002fc7996e007a6a8b32c091bc15
Saad Iqbal <saadiqbal.dev@outlook.com>
2025-09-01 18:04:18 +0500

    feat: new directory structure

--- resources/views/components/client-logo.blade.php
+++
@@ -1,7 +0,0 @@
-@props(['name', 'image', 'index'])
-
-<div class="col-6 col-md-4 col-lg-2">
-    <div class="trusted-by__logo text-center">
-        <img src="{{ asset('images/clients/' . $image) }}" alt="{{ $name }}" class="img-fluid trusted-by__logo-img" loading="lazy" width="140" height="60">
-    </div>
-</div>
